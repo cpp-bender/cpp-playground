cedb3526c971c494cb11dcb26ae19014745fe28b
cppbender <ahmetaltioglu@phoca.io>
2022-08-07 16:27:27 +0300

    feat: 1 - Intro added

---
+++ 1 - Intro/Main.cpp
@@ -0,0 +1,17 @@
+/*
+ - C++ got started with the name "C with classes" in 1979
+ - The Name C++ came later in 1982
+ - It is still the most used language (4th place in 2022 August)
+ - C++ got standardized by ISO in 1998
+ - C++ has something called STL (Standard Template Library)
+ - C++ does not include a string class but STL has
+ - Some gaming companies such as EA has their own STL library (EA has EASTL)
+ - C++ has compilers such as GCC, CLAMG, Intel C++, Microsot Visual Studio
+ - Most devs. read reference sites such as cppreference.com
+ - Last but not least it's an awesome language to make a Game Engine :)
+*/
+
+int main() 
+{
+	return 0;
+}
