cca72d7dc42eea56de5c5c7e75ae38dfdc998231
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2023-11-17 17:15:01 +0300

    feat: add 11 - Typedef

---
+++ 11 - Typedef/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+//It gives readeability to the coder
+typedef unsigned int Gevrek;
+
+int main()
+{
+	Gevrek* ahmet = new Gevrek();
+	std::cout << "Hello there" << std::endl;
+	return 0;
+}
